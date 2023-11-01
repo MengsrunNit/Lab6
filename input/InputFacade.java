36bd8f1b90a6f8b002e4d88740ad47d115a78b33
skniod <flynnnisbet@gmail.com>
2023-10-31 22:59:12 -0400

    need testing

--- input/InputFacade.java
+++ input/InputFacade.java
@@ -1,12 +1,11 @@
 package input;
 
-import static org.junit.jupiter.api.Assertions.*;
 import java.util.*;
 import geometry_objects.*;
 import geometry_objects.points.*;
 import input.builder.*;
 import input.components.*;
-import input.components.point.PointNode;
+import input.components.point.*;
 import input.parser.*;
 
 
@@ -37,19 +36,36 @@ public class InputFacade
 	 * @param fig -- a populated FigureNode object corresponding to a geometry figure
 	 * @return a point database and a set of segments
 	 */
+	
 	public static Map.Entry<PointDatabase, Set<Segment>> toGeometryRepresentation(FigureNode fig)
 	{
-		// TODO
-		PointDatabase pd = new PointDatabase();
-		for(PointNode pn: fig.getPointsDatabase().getPoints()) {
-			pd.put(pn.getName(), pn.getX(), pn.getX());
-		}
-		
-		return
+		// Calls helper methods, returns map of their products
+		PointDatabase pd = toPointDatabase(fig.getPointsDatabase().getPoints());
+		Set<Segment> segSet = toSegmentSet(fig.getSegments().getAdj());
+		return new AbstractMap.SimpleEntry<>(pd, segSet);
 		
 	}
-
-    //	
-	// TODO: implement other support methods to facilitate the toGeometryRepresentation method
-	//
+	
+	public static PointDatabase toPointDatabase(Set<PointNode> pnd) {
+		
+		//Loops through the given PND, adds each PN as Points in PointDatabase
+		PointDatabase pd = new PointDatabase();
+		for(PointNode pn: pnd) {
+			pd.put(pn.getName(), pn.getX(), pn.getX());
+		}
+		return pd;
+	}
+	public static HashSet<Segment> toSegmentSet(Map<PointNode, Set<PointNode>> ss) {
+		
+		//Loops throught the given _adjLists, making Segment objects for each value in a given key
+		HashSet<Segment> segSet = new HashSet<Segment>();
+		for (PointNode k : ss.keySet()) {
+			Set<PointNode> start = ss.get(k);
+			for (PointNode v : start) {
+				//Adds them to our Segment Set by making them Points
+				segSet.add(new Segment(new Point(k.getName(), k.getX(), k.getY()), new Point(v.getName(), v.getX(), v.getY())));
+			}
+		}
+		return segSet;
+	}
 }
