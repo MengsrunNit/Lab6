9518777dbe892248165ba0ba016a02b95136e695
skniod <flynnnisbet@gmail.com>
2023-11-01 02:22:53 -0400

    PointTest

--- tests/PointTest.java
+++ tests/PointTest.java
@@ -1,26 +1,28 @@
 package tests;
-
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.*;
-import geometry_objects.points.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import geometry_objects.*;
+import geometry_objects.points.*;
 import org.junit.jupiter.api.Test;
 
 class PointTest {
-
 	@Test
 	void lexTest() {
 		Point p1 = new Point(1, 2);
 		Point p2 = new Point(2, 1);
 		
-		//assertTrue(LexicographicOrdering(p1, p2) == -1);
+		assertEquals(Point.LexicographicOrdering(p1, p2),-1);
+		
 	}
+	
 	@Test
 	void equalsTest() {
-		Point p1 = new Point(1, 2);
-		Point p2 = new Point(2, 1);
+		Point p1 = new Point("A",1, 2);
+		Point p2 = new Point("C",2, 1);
 		
-		//assertFalse(equals(p1, p2));
+		assertEquals(p1.equals(p2), false);
+		
 	}
-
 }
